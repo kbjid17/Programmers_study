b0772cfb9095a46fad51e62c103613a8a318867c
김범주 <kbjid17@naver.com>
2022-02-23 09:02:01 +0900

    220223 Commit

--- src/BFSDFS/네트워크/Solution.java
+++ src/BFSDFS/네트워크/Solution.java
@@ -8,7 +8,7 @@ class Solution {
 	static boolean[][] check;
 	static boolean[] visit;
 	static Queue<Integer> queue = new LinkedList<Integer>();
-    static ArrayList[] lists;
+    static ArrayList<Integer>[] lists;
 	public int solution(int n, int[][] computers) {
 		check = new boolean[n][n];
 		visit = new boolean[n];
@@ -33,13 +33,13 @@ class Solution {
 	static int bfs(int n,int answer) {
 		for (int i = 0; i < n; i++) {
 			if(visit[i]) continue;
-			queue.offer(i);
+			queue.offer(i); // 넣기
 			while(!queue.isEmpty()) {
-				int a = queue.poll();
+				int a = queue.poll(); //빼기
 				visit[a] = true;
 				for (int j = 0; j < lists[a].size(); j++) {
-					if(visit[(int)lists[a].get(j)]) continue;
-					check[i][(int)lists[a].get(j)] = true;
+					if(visit[lists[a].get(j)]) continue;
+					check[i][lists[a].get(j)] = true;
 					
 					if(check[i][(int) lists[a].get(j)] && check[(int) lists[a].get(j)][i]) {
 						visit[(int) lists[a].get(j)] = true;
