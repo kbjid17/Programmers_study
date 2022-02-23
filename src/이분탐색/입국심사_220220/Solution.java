97447191be731da77bee575652a4a7e052ae072e
김범주 <kbjid17@naver.com>
2022-02-23 09:01:02 +0900

    220223 Commit - 이분탐색 알고리즘 공부

---
+++ src/이분탐색/입국심사_220220/Solution.java
@@ -0,0 +1,28 @@
+package 이분탐색.입국심사_220220;
+
+import java.util.Arrays;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        Arrays.sort(times);
+        long start = 0;
+        long end = times[times.length-1] * n;
+        
+        while(start <= end) {
+        	long mid = (start + end)/2;
+        	int sum = 0;
+        	for (int i = 0; i < times.length; i++) {
+				sum += mid/times[i];
+			}
+        	if(sum < n) {
+        		start = mid +1;
+        	}
+        	else { 
+        		end = mid-1;
+        		answer = mid;
+        	}
+        }
+        return answer;
+    }
+}
